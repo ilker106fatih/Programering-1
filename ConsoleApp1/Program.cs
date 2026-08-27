9523977c4fda53e22b49c07f08ba0a1ad83722fa
ilker106fatih <ilkermert2021@outlook.com>
2026-08-27 10:35:21 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hej vad heter du");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name} vad snygg du är idag!");
+
+Console.WriteLine("Vad är din favorit mat");
+
+string mat = Console.ReadLine();
+
+Console.ReadLine();
+
+Console.WriteLine($"Ja, {mat} det låter gott!");
+
+Console.WriteLine("Vad är din favorit färg");
+
+string färg = Console.ReadLine();
+
+Console.ReadLine();
+
+Console.WriteLine($"Ja, {färg} det är en bra färg!");
+
+Console.ReadLine();
+
+Console.WriteLine($"Du heter {name}, din favorit mat är {mat} och din favorit färg är {färg}, låter intressant!");
+
+Console.ReadLine();
